79e7957d5565ec436e8167b51b177bcc2292b287
Jonathon Chappell <jc300329@ad.jcu.edu.au>
2016-09-19 13:17:57 +1000

    Fixed Debug Errors

--- DebuggingExercises/DebugEleven3.java
+++ DebuggingExercises/DebugEleven3.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 // Two boats should be equal
 // if they hold the same number of passengers
 // and also have the same power source
@@ -5,10 +7,10 @@ public class DebugEleven3
 {
    public static void main(String args[])
    {
-      DebugRowboat redBoat = new DebugRowboat;
+      DebugRowboat redBoat = new DebugRowboat();
       DebugRowboat blueBoat = new DebugRowboat();
       System.out.print("The two boats are");
-      if(redBoat == blueBoat)
+      if(Objects.equals(redBoat.toString(), blueBoat.toString()))
          System.out.println(" equal");
       else
 	 System.out.println(" not equal");
